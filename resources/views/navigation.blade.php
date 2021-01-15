4a6e46768e8fa0144889c9d29770ea6337d5273d
damarsimple <damaralbaribin@gmail.com>
2021-01-15 22:47:25 +0700

    navigation higlihther

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,16 +1,35 @@
 <ul class="text-center text-xl">
-    <li class="text-blue-600 bg-blue-200 rounded-r-lg py-2">
-        My Drive
-    </li>
-    <li class="py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg">
-        Recent
-    </li>
-    <li class="py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg">
-        Shared With Me
-    </li>
-    <li class="py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg">
-        Bin
-    </li>
+    <a href="/dashboard">
+        <li class="{{ Route::current()->getName() == 'dashboard' ? 
+            'text-blue-600 bg-blue-200 rounded-r-lg py-2' 
+            : 
+            'py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg' }}">
+            My Drive
+        </li>
+    </a> <a href="/recent">
+        <li class="{{ Route::current()->getName() == 'recent' ? 
+            'text-blue-600 bg-blue-200 rounded-r-lg py-2' 
+            : 
+            'py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg' }}">
+            Recent
+        </li>
+    </a>
+    <a href="/shared">
+        <li class="{{ Route::current()->getName() == 'shared' ? 
+            'text-blue-600 bg-blue-200 rounded-r-lg py-2' 
+            : 
+            'py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg' }}">
+            Shared With Me
+        </li>
+    </a>
+    <a href="/bin">
+        <li class="{{ Route::current()->getName() == 'bin' ? 
+            'text-blue-600 bg-blue-200 rounded-r-lg py-2' 
+            : 
+            'py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg' }}">
+            Bin
+        </li>
+    </a>
     <hr>
     <li class="py-2 hover:bg-gray-100 cursor-pointer rounded-r-lg px-3">
         Storage (90%)
